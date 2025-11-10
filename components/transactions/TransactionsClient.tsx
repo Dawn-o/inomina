ec2f46bcce40d3b81920c5e2595ddfbce9e22957
Dawn-o <ruselaq@gmail.com>
2025-11-10 23:50:17 +0800

    temp: make website refresh everytime users adding data Temporarily reload page after adding transaction

--- components/transactions/TransactionsClient.tsx
+++ components/transactions/TransactionsClient.tsx
@@ -101,6 +101,7 @@ export function TransactionsClient({
         defaultType={selectedType}
         onSubmit={() => {
           setOpen(false);
+          window.location.reload();
         }}
       />
     </>
